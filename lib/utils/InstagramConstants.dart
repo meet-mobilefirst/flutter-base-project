0805960fe79c5439e22e8e7064803383b9df646c
meet-mobilefirst <78348094+meet-mobilefirst@users.noreply.github.com>
2021-08-05 17:04:21 +0530

    Delete InstagramConstants.dart

--- lib/utils/InstagramConstants.dart
+++
@@ -1,5 +0,0 @@
-class InstagramConstants {
-  static const igClientId = "974666943360894";
-  static const igClientSecret = "335ac1ce17490bf581da8cdb57aa2a26";
-  static const igRedirectURI = "https://boozlr.com/";
-}
